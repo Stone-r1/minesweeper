3fc92c599e388b5425753609ac839a471d78739f
stoney <sarkisyanmihail11@gmail.com>
2025-06-03 19:27:35 +0400

    feat(OOP): improved readability via separating Board class

--- main.cpp
+++ main.cpp
@@ -56,6 +56,192 @@ public:
     }
 };
 
+class Board {
+private:
+    vector<vector<Cell>> grid;
+    bool firstClick;
+
+public:
+    Board() {
+        grid.assign(MAX, vector<Cell>(MAX));
+        firstClick = true;
+    }
+
+    void draw() {
+        for (int row = 0; row < MAX; row++) {
+            for (int column = 0; column < MAX; column++) {
+                auto& refToCell = grid[row][column];
+                refToCell.x = column * LENGTH;
+                refToCell.y = row * LENGTH;
+
+                int drawX = refToCell.x + 15;
+                int drawY = refToCell.y + 145;
+
+                // 15 px margin on the right and the left
+                DrawRectangle(drawX, drawY, LENGTH, LENGTH, GRAY);
+                DrawRectangle(drawX + 2, drawY + 2, LENGTH - 4, LENGTH - 4, BLACK);
+
+                if (refToCell.activated) {
+                    if (refToCell.isEmpty()) {
+                        DrawRectangle(drawX + 2, drawY + 2, LENGTH - 4, LENGTH - 4, LIGHTGRAY); // TEMPORARY
+                        continue;
+                    }
+                    DrawText(TextFormat("%d", refToCell.status), refToCell.x + 30, refToCell.y + 160, 25, GREEN);
+                } else if (refToCell.isFlagged) {
+                    DrawText(TextFormat("%s", "#"), refToCell.x + 30, refToCell.y + 160, 25, RED);
+                }
+            }
+        }
+    }
+
+    void restart() {
+        for (int i = 0; i < MAX; i++) {
+            for (int j = 0; j < MAX; j++) {
+                grid[i][j].reset();
+            }
+        }
+
+        currentFlagAmount = MINECOUNT;
+        elapsedTime = 0;
+        isStopwatchRunning = false;
+        firstClick = true;
+    }
+
+    void generateMines(int x, int y) {
+        int currentMineCount = 0;
+        while (currentMineCount < MINECOUNT) {
+            int row = GetRandomValue(0, MAX - 1);
+            int column = GetRandomValue(0, MAX - 1);
+
+            // skip the safe cell and its neighbors
+            if (abs(row - x) <= 1 && abs(column - y) <= 1) {
+                continue;
+            }
+
+            if (grid[row][column].isMine()) {
+                continue;
+            }
+
+            grid[row][column].status = MINE;
+            currentMineCount++;
+        }    
+    }
+
+    void generateNumbers() {
+        for (int row = 0; row < MAX; row++) {
+            for (int column = 0; column < MAX; column++) {
+                int mines = 0;
+
+                for (int d = 0; d < 8; d++) {
+                    int newR = row + rowDir[d];
+                    int newC = column + colDir[d];
+
+                    if (newR < 0 || newC < 0 || newR >= MAX || newC >= MAX) {
+                        continue;
+                    }
+
+                    if (grid[newR][newC].isMine()) {
+                        mines++;
+                    }
+                }
+                
+                if (!grid[row][column].isMine()) {
+                    grid[row][column].status = mines;
+                }
+            }
+        }
+    }
+
+    void bfs(int x, int y) {
+        queue<pair<int, int>> q;
+        q.push({x, y});
+        
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            grid[r][c].activate();
+
+            for (int d = 0; d < 8; d++) {
+                int newR = r + rowDir[d];
+                int newC = c + colDir[d];
+                
+                if (newR < 0 || newC < 0 || newR >= MAX || newC >= MAX || grid[newR][newC].activated) {
+                    continue;
+                }
+
+                grid[newR][newC].activate();
+                if (grid[newR][newC].isEmpty()) {
+                    q.push({newR, newC});
+                } else if (grid[newR][newC].isMine()) {
+                    if (!grid[newR][newC].isFlagged) {
+                        restart(); 
+                        return;
+                    } else {
+                        grid[newR][newC].deactivate();
+                    }
+                }
+            }
+        }
+    }
+
+    void handleLeftClick(int mouseX, int mouseY) {
+        int statusBarX = mouseX;
+        int statusBarY = mouseY;
+
+        if (statusBarX >= 0 && statusBarX < statusBarHeight && statusBarY >= 0 && statusBarY < WIDTH) {
+            int x1 = 10, x2 = statusBarHeight - 20;
+            int y1 = x1, y2 = x2;
+            if (statusBarY < x2 && statusBarY > x1 && statusBarX < y2 && statusBarX > y1) {
+                restart();
+                return;
+            }
+        }
+
+        int x = (mouseX - 15) / LENGTH;
+        int y = (mouseY - 155) / LENGTH;
+
+        if (x < 0 || y < 0 || x >= MAX || y >= MAX || grid[y][x].isFlagged) return;
+
+        grid[y][x].activate();
+        if (firstClick) {
+            generateMines(y, x);
+            generateNumbers();
+            firstClick = false;
+            startTime = GetTime();
+            isStopwatchRunning = true;
+        }
+
+        if (grid[y][x].isEmpty()) {
+            bfs(y, x);
+        } else if (grid[y][x].isMine()) {
+            restart();
+        } else {
+            int minesNum = grid[y][x].status;
+            int flagsFound = 0;
+            for (int d = 0; d < 8; d++) {
+                int newR = y + rowDir[d];
+                int newC = x + colDir[d];
+                if (newR < 0 || newC < 0 || newR >= MAX || newC >= MAX) continue;
+                if (grid[newR][newC].isFlagged) flagsFound++;
+            }
+            if (flagsFound >= minesNum) {
+                bfs(y, x);
+            }
+        }
+    }
+
+    void handleRightClick(int mouseX, int mouseY) {
+        int x = (mouseX - 15) / LENGTH;
+        int y = (mouseY - 155) / LENGTH;
+
+        if (x >= 0 && y >= 0 && x < MAX && y < MAX) {
+            grid[y][x].toggleFlag();
+            currentFlagAmount += grid[y][x].isFlagged ? -1 : 1;
+        }
+    }
+};
+
 void drawStatusBar() {
     DrawRectangle(0, 0, WIDTH, statusBarHeight, RED);
 
@@ -91,217 +277,30 @@ void drawStatusBar() {
     DrawText(buffer, flagTextX, flagTextY, fontSize, GREEN);
 }
 
-void drawCells(vector<vector<Cell>>& grid) {
-    for (int row = 0; row < MAX; row++) {
-        for (int column = 0; column < MAX; column++) {
-            auto& refToCell = grid[row][column];
-            refToCell.x = column * LENGTH;
-            refToCell.y = row * LENGTH;
-
-            int drawX = refToCell.x + 15;
-            int drawY = refToCell.y + 145;
-
-            // 15 px margin on the right and the left
-            DrawRectangle(drawX, drawY, LENGTH, LENGTH, GRAY);
-            DrawRectangle(drawX + 2, drawY + 2, LENGTH - 4, LENGTH - 4, BLACK);
-
-            if (refToCell.activated) {
-                if (refToCell.isEmpty()) {
-                    DrawRectangle(drawX + 2, drawY + 2, LENGTH - 4, LENGTH - 4, LIGHTGRAY); // TEMPORARY
-                    continue;
-                }
-                DrawText(TextFormat("%d", refToCell.status), refToCell.x + 30, refToCell.y + 160, 25, GREEN);
-            } else if (refToCell.isFlagged) {
-                DrawText(TextFormat("%s", "#"), refToCell.x + 30, refToCell.y + 160, 25, RED);
-            }
-        }
-    }
-}
-
-void generateMines(vector<vector<Cell>>& grid, int x, int y) {
-    int currentMineCount = 0;
-    while (currentMineCount < MINECOUNT) {
-        int row = GetRandomValue(0, MAX - 1);
-        int column = GetRandomValue(0, MAX - 1);
-
-        // skip the safe cell and its neighbors
-        if (abs(row - x) <= 1 && abs(column - y) <= 1) {
-            continue;
-        }
-
-        if (grid[row][column].isMine()) {
-            continue;
-        }
-
-        grid[row][column].status = MINE;
-        currentMineCount++;
-    }
-}
-
-void generateNumbers(vector<vector<Cell>>& grid) {
-    for (int row = 0; row < MAX; row++) {
-        for (int column = 0; column < MAX; column++) {
-            int mines = 0;
-
-            for (int d = 0; d < 8; d++) {
-                int newR = row + rowDir[d];
-                int newC = column + colDir[d];
-
-                if (newR < 0 || newC < 0 || newR >= MAX || newC >= MAX) {
-                    continue;
-                }
-
-                if (grid[newR][newC].isMine()) {
-                    mines++;
-                }
-            }
-            
-            if (!grid[row][column].isMine()) {
-                grid[row][column].status = mines;
-            }
-        }
-    }
-}
-
-void restartTheGame(vector<vector<Cell>>& grid) {
-    for (int i = 0; i < MAX; i++) {
-        for (int j = 0; j < MAX; j++) {
-            grid[i][j].reset();
-        }
-    }
-
-    currentFlagAmount = MINECOUNT;
-    elapsedTime = 0;
-    isStopwatchRunning = false;
-}
-
-void manageStatusBarActivities(int r, int c, vector<vector<Cell>>& grid, bool& firstClick) {
-    int x1 = 10, x2 = statusBarHeight - 20;
-    int y1 = x1, y2 = x2;
-
-    if (r < x2 && r > x1 && c < y2 && c > y1) {
-        restartTheGame(grid);
-        firstClick = true;
-    }
-}
-
-void bfs(vector<vector<Cell>>& grid, int x, int y, bool& firstClick) {
-    queue<pair<int, int>> q;
-    q.push({x, y});
-    
-    while (!q.empty()) {
-        auto [r, c] = q.front();
-        q.pop();
-
-        grid[r][c].activate();
-
-        for (int d = 0; d < 8; d++) {
-            int newR = r + rowDir[d];
-            int newC = c + colDir[d];
-            
-            if (newR < 0 || newC < 0 || newR >= MAX || newC >= MAX || grid[newR][newC].activated) {
-                continue;
-            }
-
-            grid[newR][newC].activate();
-            if (grid[newR][newC].isEmpty()) {
-                q.push({newR, newC});
-            } else if (grid[newR][newC].isMine()) {
-                if (!grid[newR][newC].isFlagged) {
-                    restartTheGame(grid);
-                    firstClick = true;
-                    return;
-                } else {
-                    grid[newR][newC].deactivate();
-                }
-            }
-        }
-    }
-}
-
 int main() {
     InitWindow(WIDTH, HEIGHT, "testing");
     SetTargetFPS(60);
 
-    vector<vector<Cell>> grid(MAX, vector<Cell>(MAX));
-    bool firstClick = true;
-
+    Board board;
     while(!WindowShouldClose()) {
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            Vector2 mouse = GetMousePosition();
-
-            int statusBarX = mouse.x;
-            int statusBarY = mouse.y;
-
-            if (statusBarX >= 0 && statusBarX < statusBarHeight && statusBarY >= 0 && statusBarY < WIDTH) {
-                manageStatusBarActivities(statusBarY, statusBarX, grid, firstClick);
-            }
-
-            int x = (mouse.x - 15) / LENGTH;
-            int y = (mouse.y - 155) / LENGTH;
-            
-            if (x >= 0 && y >= 0 && x < MAX && y < MAX && !grid[y][x].isFlagged) {
-                grid[y][x].activate();
-                if (firstClick) {
-                    generateMines(grid, y, x);
-                    generateNumbers(grid);
-                    firstClick = false;
-
-                    startTime = GetTime();
-                    isStopwatchRunning = true;
-                }
-                
-                if (grid[y][x].isEmpty()) {
-                    bfs(grid, y, x, firstClick);
-                } else if (grid[y][x].isMine()) {
-                    // ADD GAMEOVER SCREEN LATER
-                    restartTheGame(grid);
-                    firstClick = true;
-                } else {
-                    int minesNum = grid[y][x].status;
-                    int flagsFound = 0;
-
-                    for (int d = 0; d < 8; d++) {
-                        int newR = y + rowDir[d];
-                        int newC = x + colDir[d];
-
-                        if (newR < 0 || newC < 0 || newR >= MAX || newC >= MAX) {
-                            continue;
-                        } 
-                        if (grid[newR][newC].isFlagged) {
-                            flagsFound++;
-                        }
-                    }
-
-                    if (flagsFound >= minesNum) {
-                        bfs(grid, y, x, firstClick);
-                    }
-                }
-            }
-        } else if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
-            Vector2 mouse = GetMousePosition();
-            int x = (mouse.x - 15) / LENGTH;
-            int y = (mouse.y - 155) / LENGTH;
-
-            if (x >= 0 && y >= 0 && x < MAX && y < MAX) {
-                grid[y][x].toggleFlag();
-                if (grid[y][x].isFlagged) {
-                    currentFlagAmount--;
-                } else {
-                    currentFlagAmount++;
-                }
-            }
-        }
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
 
         if (isStopwatchRunning) {
             elapsedTime = GetTime() - startTime;
         }
 
-        ClearBackground(RAYWHITE);
-        
-        BeginDrawing();
-            drawStatusBar();
-            drawCells(grid);
+        drawStatusBar();
+        board.draw();
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            Vector2 mouse = GetMousePosition();
+            board.handleLeftClick(mouse.x, mouse.y);
+        } else if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+            Vector2 mouse = GetMousePosition();
+            board.handleRightClick(mouse.x, mouse.y);
+        }
+
         EndDrawing();
     }
     CloseWindow();
